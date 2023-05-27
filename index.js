bfc8d8ac9e41a2aa183c883f7658aebd2d292aed
mdsharukh123s <mdshahrukh.sk.b@gmai.com>
2023-05-27 22:41:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,170 @@
+
+var sound = new Audio("https://www.freespecialeffects.co.uk/soundfx/animals/duck1.wav");
+		sound.loop = true;
+
+
+
+
+var h2= document.getElementById('clock');
+
+// display live time by the second
+
+function currentTime() {
+    let date = new Date(); 
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session = "AM";
+  
+    if(hh === 0){
+        hh = 12;
+    }
+    if(hh > 12){
+        hh = hh - 12;
+        session = "PM";
+     }
+  
+     hh = (hh < 10) ? "0" + hh : hh;
+     mm = (mm < 10) ? "0" + mm : mm;
+     ss = (ss < 10) ? "0" + ss : ss;
+      
+     let time = hh + ":" + mm + ":" + ss + " " + session;
+  
+    document.getElementById("clock").innerText = time; 
+    let t = setTimeout(function(){ currentTime() }, 1000);
+  }
+  
+  currentTime();
+
+//
+
+
+
+
+
+
+
+function addZero(time){
+    return(time<10)? "0"+time:time;
+}
+function hoursMenu(){
+
+	var select = document.getElementById('alarmhrs');
+	var hrs = 12;
+
+	for (i=1; i <= hrs; i++) {
+		select.options[select.options.length] = new Option( i < 10 ? "0" + i : i, i);
+		
+	}
+}
+hoursMenu();
+
+function minMenu(){
+
+	var select = document.getElementById('alarmmins');
+	var min= 59;
+
+	for (i=0; i <= min; i++) {
+		select.options[select.options.length] = new Option( i < 10 ? "0" + i : i, i);
+		
+	}
+}
+minMenu();
+
+function secMenu(){
+
+	var select = document.getElementById('alarmsecs');
+	var sec = 59;
+
+	for (i=0; i <= sec; i++) {
+		select.options[select.options.length] = new Option( i < 10 ? "0" + i : i, i);
+		
+	}
+}
+secMenu();
+
+///
+
+
+
+
+
+function alarmset() {
+
+	var hr = document.getElementById('alarmhrs');
+	
+	var min = document.getElementById('alarmmins');
+	
+	var sec = document.getElementById('alarmsecs');
+	
+	var ap = document.getElementById('ampm');
+    
+
+    var selectedHour = hr.options[hr.selectedIndex].value;
+    var selectedMin = min.options[min.selectedIndex].value;
+    var selectedSec = sec.options[sec.selectedIndex].value;
+    var selectedAP = ap.options[ap.selectedIndex].value;
+
+    var alarmTime = addZero(selectedHour) + ":" + addZero(selectedMin) + ":" + addZero(selectedSec) + selectedAP;
+    console.log('alarmTime:' + alarmTime);
+
+    document.getElementById('alarmhrs').disabled = true;
+	document.getElementById('alarmmins').disabled = true;
+	document.getElementById('alarmsecs').disabled = true;
+	document.getElementById('ampm').disabled = true;
+
+
+//when alarmtime is equal to currenttime then play a sound
+	var h2 = document.getElementById('clock');
+
+/*function to calcutate the current time 
+then compare it to the alarmtime and play a sound when they are equal
+*/
+
+setInterval(() => {
+
+	var date = new Date();
+
+	var hours = (12 - (date.getHours()));
+	// var hours = date.getHours();
+	var minutes = date.getMinutes();
+
+	var seconds = date.getSeconds();
+
+	var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
+
+
+	//convert military time to standard time
+	if (hours < 0) {
+		hours = hours * -1;
+	} else if (hours == 00) {
+		hours = 12;
+	} else {
+		hours = hours;
+	}
+
+	var currentTime = h2.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + "" + ampm;
+
+
+	if (alarmTime == currentTime) {
+		sound.play();
+	}
+
+},1000);
+
+
+	// console.log('currentTime:' + currentTime);	
+
+}
+
+
+function alarmclear() {
+
+	document.getElementById('alarmhrs').disabled = false;
+	document.getElementById('alarmmins').disabled = false;
+	document.getElementById('alarmsecs').disabled = false;
+	document.getElementById('ampm').disabled = false;
+	sound.pause();
+}
+
+
